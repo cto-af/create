f50718ae6c1dc59de72479365321f0e6e25cb129
Joe Hildebrand <joe-github@cursive.net>
2024-10-07 16:48:20 -0600

    Update dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
 import es6 from '@cto.af/eslint-config/es6.js';
-import mod from '@cto.af/eslint-config/module.js';
 import ts from '@cto.af/eslint-config/ts.js';
 
 export default [
   ...es6,
-  ...mod,
   ...ts,
 ];
